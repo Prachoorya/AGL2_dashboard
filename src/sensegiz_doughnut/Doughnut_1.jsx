64815ba51f636912cfdfa6a7a1d4fb0e7a3309d8
Prachoorya Preeyambada <82390219+Prachoorya@users.noreply.github.com>
2024-04-17 14:36:34 +0530

    Add files via upload

---
+++ src/sensegiz_doughnut/Doughnut_1.jsx
@@ -0,0 +1,152 @@
+import React, {useState} from "react";
+import { ResponsivePie } from "@nivo/pie";
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
+import { sensegizFrontendDoughnutData as data } from "../../data/data";
+import { useNavigate } from "react-router-dom";
+import SensegizApplication_1 from "../../table/sensegiz/frontend_applications";
+import {Modal, Box} from "@mui/material";
+
+const Doughnut1 = () => {
+    const navigate = useNavigate();
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [showPopUp, setShowPopUp] = useState(false);
+    const [selectedFrontend, setSelectedFrontend] = useState('');
+
+    const handlePieClick = (pie) => {
+        const frontendTechnology = pie.id;
+        setSelectedFrontend(frontendTechnology);
+        setShowPopUp(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopUp(false);
+    };
+
+    return (
+     <>
+      <ResponsivePie
+      data={data}
+      theme={{
+        axis: {
+          domain: {
+            line: {
+              stroke: colors.grey[100],
+            },
+          },
+          legend: {
+            text: {
+              fill: colors.grey[100],
+            },
+          },
+          ticks: {
+            line: {
+              stroke: colors.grey[100],
+              strokeWidth: 1,
+            },
+            text: {
+              fill: colors.grey[100],
+            },
+          },
+        },
+        legends: {
+          text: {
+            fill: colors.grey[100],
+          },
+        },
+      }}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 0.2]],
+      }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor={colors.grey[100]}
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: "color" }}
+      enableArcLabels={false}
+      arcLabelsRadiusOffset={0.4}
+      arcLabelsSkipAngle={7}
+      arcLabelsTextColor={{
+        from: "color",
+        modifiers: [["darker", 2]],
+      }}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+      ]}
+      legends={[
+        {
+          anchor: "bottom",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: 56,
+          itemsSpacing: 60,
+          itemWidth: 100,
+          itemHeight: 18,
+          itemTextColor: "#999",
+          itemDirection: "left-to-right",
+          itemOpacity: 1,
+          symbolSize: 18,
+          symbolShape: "circle",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemTextColor: "#000",
+              },
+            },
+          ],
+        },
+      ]}
+      onClick={handlePieClick}
+      tooltip={({ datum }) => (
+        <div style={{ padding: "5px", background: "#000", border: "1px solid #ccc" }}>
+          <p>{datum.id}</p>
+          <p>: {datum.value}</p>
+        </div>
+      )}
+    />
+
+   <Modal open={showPopUp} onClose={handleClosePopup}>
+        <Box
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                bgcolor: 'background.paper',
+                boxShadow: 24,
+                p: 4,
+            }}
+        >
+            <SensegizApplication_1 selectedFrontend={selectedFrontend} navigate={navigate} />
+        </Box>
+     </Modal>
+    </>
+    );
+};
+
+export default Doughnut1;
---
+++ src/xane_barcharts/Barchart_2.jsx
@@ -0,0 +1,166 @@
+import React, {useState} from "react";
+import { useTheme } from "@mui/material";
+import { ResponsiveBar } from "@nivo/bar";
+import { tokens} from "../../theme";
+import { xaneBackendData as data } from "../../data/data";
+import { useNavigate } from "react-router-dom";
+import XaneApplication_2 from "../../table/xane/backend_applications";
+import { Modal, Box } from "@mui/material";
+
+const BarChart2 = () => {
+    const navigate = useNavigate();
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [showPopUp, setShowPopUp] = useState(false);
+    const [selectedBackend, setSelectedBackend] = useState('');
+
+    const handleBarClick = (bar) => {
+        const backendTechnology = bar.indexValue;
+        setSelectedBackend(backendTechnology);
+        setShowPopUp(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopUp(false);
+    };
+
+    return (
+     <>
+        <ResponsiveBar
+        data={data}
+
+        theme={{
+            axis: {
+                domain: {
+                    line: {
+                        stroke: colors.grey[100]
+                    }
+                },
+                legend: {
+                    text: {
+                        fill: colors.grey[100]
+                    }
+                },
+                ticks: {
+                    line: {
+                        stroke: colors.grey[100],
+                        strokeWidth: 1
+                    },
+                    text: {
+                        fill: colors.grey[100]
+                    },
+                },
+            },
+            legends: {
+                text: {
+                    fill: colors.grey[100],
+                },
+            },
+        }}
+
+        keys={[
+            'legend',
+        ]}
+        indexBy="backendTechnology"
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        padding={0.3}
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        colors={['#EAFB84']}
+        colorBy="index"
+        borderColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    1.6
+                ]
+            ]
+        }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Backend Technology',
+            legendPosition: 'middle',
+            legendOffset: 32,
+            truncateTickAt: 0
+        }}
+        axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Number',
+            legendPosition: 'middle',
+            legendOffset: -40,
+            truncateTickAt: 0
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    1.6
+                ]
+            ]
+        }}
+        legends={[
+            {
+                dataFrom: 'keys',
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: 'left-to-right',
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+        role="application"
+        ariaLabel="Nivo bar chart demo"
+        barAriaLabel={e=>e.id+": "+e.formattedValue+" in backendTechnology: "+e.indexValue}
+        onClick={handleBarClick}
+        tooltip={({ legend, value, data }) => (
+            <div style={{ padding: "5px", background: "#000", border: "1px solid #ccc" }}>
+                <p>{data.backendTechnology}</p>
+                <p>{legend}: {value}</p>
+            </div>
+        )}
+    />
+
+     <Modal open={showPopUp} onClose={handleClosePopup}>
+        <Box
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                bgcolor: 'background.paper',
+                boxShadow: 24,
+                p: 4,
+            }}
+        >
+            <XaneApplication_2 selectedBackend={selectedBackend} navigate={navigate} />
+        </Box>
+     </Modal>
+    </>
+    );
+};
+
+export default BarChart2;
